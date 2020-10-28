1aa1ff89e1e32f9a90a155fe250a33c10817b2f5
Sophie-top <71389432+Sophie-top@users.noreply.github.com>
2020-10-28 17:41:52 +0300

    Add files via upload

---
+++ hero.py
@@ -0,0 +1,19 @@
+mypet = {
+    "имя": "Анатолий",
+    "портрет": 
+    "возраст": 32
+    "сон": 100
+    "счастье": 100
+    "сытость": 0
+}
+
+
+
+def print_pet(mypet):
+    for key, value in pet.items():
+    	print(key, value)
+
+def 
+
+
+print_pet(mypet)
