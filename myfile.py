19c117fd6a2ddf4f5ee8a997702ecc977a795227
Sophie-top <71389432+Sophie-top@users.noreply.github.com>
2020-12-19 13:41:39 +0300

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,31 @@
+class Orc():
+    #атрибуты класса
+    name = ""
+    mood = ""
+    HP = 100
+    inventory = [] 
+
+def __init__(self, name, mood, inventoty):
+    print("Создан орк по имени", HP name)
+
+
+#методы класса
+    def introduce(self):
+    print(self.name)
+    print(self.mood)
+    print(self.HP)
+    print( *self.inventory, sep = ", ")
+
+    def heal(self, add_HP):
+        sels.HP += add_hp
+
+    def full_heal(self):
+        self.hp += 100 - self.hp
+
+
+
+
+
+first_orc = Orc("Василий", "бычное", 100, [])
+
+first_orc.introduce()
